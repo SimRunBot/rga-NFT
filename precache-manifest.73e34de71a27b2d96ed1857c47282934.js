ced8d6dbdd7f7a46af5b29e35d71d5dfd28284c5
simon <simon.r.both@gmail.com>
2021-05-14 14:33:24 +0200

    Updates

---
+++ precache-manifest.73e34de71a27b2d96ed1857c47282934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f5e548ba6b7961a0827b2fa525b90c",
+    "url": "/rga-NFT/index.html"
+  },
+  {
+    "revision": "5a703ae15c3602932f4a",
+    "url": "/rga-NFT/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f39870192b3da1a9b49d",
+    "url": "/rga-NFT/static/js/2.61ea055a.chunk.js"
+  },
+  {
+    "revision": "732254eaf45107d7b92f6eea5f178e8f",
+    "url": "/rga-NFT/static/js/2.61ea055a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a703ae15c3602932f4a",
+    "url": "/rga-NFT/static/js/main.ed0f7123.chunk.js"
+  },
+  {
+    "revision": "161a773f26c4cc50c99d",
+    "url": "/rga-NFT/static/js/runtime-main.59322958.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rga-nft/precache-manifest.3e2f182fb2637b08e06fcf8fdc4fb4b4.js"
+  "/rga-NFT/precache-manifest.73e34de71a27b2d96ed1857c47282934.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rga-nft/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rga-NFT/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
